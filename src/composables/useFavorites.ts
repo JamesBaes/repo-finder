46d1c09aba22ef80e0c0b8930385bbb57dd5213d
James Baes <jeb5002@shaw.ca>
2026-03-22 17:11:21 -0600

    feature: create useFavorite composable using localStorage

---
+++ src/composables/useFavorites.ts
@@ -0,0 +1,26 @@
+import { ref } from "vue";
+import type { Repository } from "@/types";
+
+// composable for managing favorite repositories using localStorage
+export const useFavorites = () => {
+
+  // load favorites
+  const stored = localStorage.getItem('favorites')
+  const favorites = ref<Repository[]>(stored ? JSON.parse(stored) : [])
+
+  const addFavorite = (repo: Repository) => {
+    favorites.value = [...favorites.value, repo]
+    localStorage.setItem("favorites", JSON.stringify(favorites.value))
+  } 
+
+  const removeFavorite = (repo: Repository) => {
+    favorites.value = favorites.value.filter((favorite) => favorite.id !== repo.id) // retains all favorites that aren't matching the target repo
+    localStorage.setItem("favorites", JSON.stringify(favorites.value))
+  }
+
+  const isFavorite = (repo: Repository): boolean => {
+    return favorites.value.some(favorite => favorite.id === repo.id)
+  }
+
+  return { favorites, addFavorite, removeFavorite, isFavorite }
+}
